7745e462b77e944e1112a951ee4ff94611cf99d8
Wyatt <self@wyattbaldwin.com>
2012-12-03 15:20:07 -0800

    Convert to distribution (add setup.py)

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='glineenc',
+    version='1.0',
+    description='Convert lat/long pairs to Base64 encoding for Google Maps',
+    license='MIT',
+    author='Wyatt Lee Baldwin',
+    author_email='wyatt.lee.baldwin@gmail.com',
+    keywords='Google Maps',
+    url='https://bitbucket.org/wyatt/glineenc',
+    py_modules=('glineenc',),
+)
