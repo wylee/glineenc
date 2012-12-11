071fb389780ca12bad1ed21e80768dd4d612dcb3
Rodrigo Machado <rcmachado@gmail.com>
2012-12-11 17:55:42 -0200

    Reorganize code and tests in separate directories
    Remove tests from main file and add to tests package. Also
    create a top-level directory to hold the files.
    Tests can run with nosetests command.

--- glineenc/glineenc.py
+++ glineenc/glineenc.py
@@ -180,37 +180,3 @@ def compute_level(distance):
     while distance < zoom_level_breaks[level]:
         level += 1
     return level
-
-def test_encode_negative():
-    f = -179.9832104
-    assert encode_lat_or_long(f, 0)[0] == '`~oia@'
-    
-    f = -120.2
-    assert encode_lat_or_long(f, 0)[0] == '~ps|U'
-
-def test_encode_positive():
-    f = 38.5
-    assert encode_lat_or_long(f, 0)[0] == '_p~iF'
-
-def test_encode_one_pair():
-    pairs = [(38.5, -120.2)]
-    expected_encoding = '_p~iF~ps|U', 'B'
-    assert encode_pairs(pairs) == expected_encoding
-
-def test_encode_pairs():
-    pairs = (
-        (38.5, -120.2),
-        (40.7, -120.95),
-        (43.252, -126.453),
-        (40.7, -120.95),
-    )
-    expected_encoding = '_p~iF~ps|U_ulLnnqC_mqNvxq`@~lqNwxq`@', 'BBBB'
-    assert encode_pairs(pairs) == expected_encoding
-    
-    pairs = (
-        (37.4419, -122.1419),
-        (37.4519, -122.1519),
-        (37.4619, -122.1819),
-    )
-    expected_encoding = 'yzocFzynhVq}@n}@o}@nzD', 'B@B'
-    assert encode_pairs(pairs) == expected_encoding    
